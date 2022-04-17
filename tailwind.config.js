2776a43529b3c1ea070b599e4e9a29a2987223f1
Rupasr <101166941+Rupasr@users.noreply.github.com>
2022-04-17 20:18:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        my_coral: '#d8d8d8',
+        my_grey: '#4a4a4a',
+        my_lightGrey: '#eaebec'
+      }
+    },
+  },
+  plugins: [],
+}
